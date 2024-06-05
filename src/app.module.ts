ecc3c713fe1f2de72162cc4d6b56d4b06adb0b0e
Fiuse <Fiw0202@hotmail.com>
2024-06-05 21:13:48 +0700

    First-commit

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://fiw0202:ofsnl1qQLv1g6wV0@blogpostcluster.yabjhhf.mongodb.net/?retryWrites=true&w=majority&appName=BlogPostCluster',
+    ),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
