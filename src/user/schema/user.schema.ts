144171adf46e3c145d4cdab9a657e09f646fd826
Fiuse <Fiw0202@hotmail.com>
2024-06-06 09:53:40 +0700

    เพิ่ม imterface
    - implement interface ให้กับ Schema user

--- src/user/schema/user.schema.ts
+++ src/user/schema/user.schema.ts
@@ -1,11 +1,12 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 import { Document } from 'mongoose';
+import { IRespUser } from '../interface';
 
 // export type userDocument = HydratedDocument<user>;
 export type userDocument = user & Document;
 @Schema({ collection: 'users' })
-export class user {
+export class user implements IRespUser {
   @Prop()
   firstName: string;
 
